133a9cb1b9447e9e33bcb35e5a1c10bb44df6011
Bruno Moleta <brunomoleta@pm.me>
2024-08-06 13:45:06 -0300

    :sparkle: feat: módulo de Fluxo

---
+++ m-10-controle-fluxo/src/Fluxo.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Fluxo {
+    private final double[] notas = new double[4];
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < notas.length; i++) {
+            if (i == notas.length-1) {
+                System.out.println("Qual foi a sua última nota?");
+            } else {
+                System.out.println("Qual foi a sua " + (i+1) + "ª nota?");
+            }
+            this.notas[i] = sc.nextDouble();
+        }
+
+        sc.close();
+    }
+
+    private double calcularMedia() {
+        double sum = 0;
+        for (double nota : notas) {
+            sum += nota;
+        }
+        return sum / notas.length;
+    }
+
+    private String resultadoEstudante() {
+        if (this.calcularMedia() >= 7) {
+            return "foi aprovado";
+        } else if (this.calcularMedia() >= 5) {
+            return "vai para recuperação";
+        } else {
+            return "foi reprovado";
+        }
+    }
+
+    public void imprimirResultadoEstudante() {
+        System.out.println("Te informo que você " + this.resultadoEstudante() + ". Isso porque sua média foi: " + this.calcularMedia());
+    }
+}
+
---
+++ m-10-controle-fluxo/src/TesteFluxo.java
@@ -0,0 +1,7 @@
+public class TesteFluxo {
+    public static void main(String[] args) {
+        Fluxo fluxo = new Fluxo();
+        fluxo.start();
+        fluxo.imprimirResultadoEstudante();
+    }
+}
