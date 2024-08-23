5c9bff7d9febde27bd6bf16a6e08e2a51e87ca2c
Bruno Moleta <brunomoleta@pm.me>
2024-08-23 11:47:08 -0300

    :sparkles: feat:m-18-reflections

---
+++ m-18-annotations/src/Reflection.java
@@ -0,0 +1,16 @@
+
+public class Reflection {
+    public static void main(String[] args) {
+        try {
+            Class<?> clazz = Class.forName("ExemploTabela");
+            Tabela tabelaAnnotation = clazz.getAnnotation(Tabela.class);
+            String nomeTabela = tabelaAnnotation.nome();
+
+            System.out.println("O nome da tabela é: " + nomeTabela);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Classe não encontrada.");
+            e.printStackTrace();
+        }
+    }
+}
