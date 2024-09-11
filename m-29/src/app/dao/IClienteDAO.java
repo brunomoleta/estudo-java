7288cb5453ce3dc17e35102e0f15cf0441677356
Bruno Moleta <63274069+brunomoleta@users.noreply.github.com>
2024-09-11 17:29:10 -0300

    :sparkles: feat: m-29 JDBC => SQL

---
+++ m-29/src/app/dao/IClienteDAO.java
@@ -0,0 +1,9 @@
+package app.dao;
+
+import app.domain.Cliente;
+
+public interface IClienteDAO {
+    public Integer cadastrar(Cliente cliente) throws Exception;
+    public Cliente consultar(String codigo) throws Exception;
+    public Integer excluir(Cliente clienteBD) throws Exception;
+}
